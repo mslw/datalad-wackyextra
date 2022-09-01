ceff0f201ae1c1838ed84755851c86675b2dd1dd
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2022-09-01 18:52:13 +0200

    Add an nbib extractor

--- datalad_wackyextra/extractors/citations.py
+++ datalad_wackyextra/extractors/citations.py
@@ -1,9 +1,11 @@
 """Metadata extractor for citation files"""
 
+from datetime import datetime
 from importlib.metadata import version
 from pathlib import Path
 from uuid import UUID
 
+import nbib
 import rispy
 
 from datalad_metalad.extractors.base import (
@@ -39,6 +41,56 @@ class RisExtractor(DatasetMetadataExtractor):
         refs = []
         for f in self._cite_files:
             refs.extend(rispy.load(f, encoding="utf-8"))
+        return refs
+
+    def extract(self, _=None) -> ExtractorResult:
+        return ExtractorResult(
+            extractor_version=self.get_version(),
+            extraction_parameter=self.parameter or {},
+            extraction_success=True,
+            datalad_result_dict={
+                "type": "dataset",
+                "status": "ok",
+            },
+            immediate_data={
+                "id": self.dataset.id,
+                "refcommit": self.dataset.repo.get_hexsha(),
+                "refs": self._read_files(),
+            }
+        )
+
+
+class NbibExtractor(DatasetMetadataExtractor):
+
+    def get_id(self) -> UUID:
+        return UUID("4b898c36-3ff0-4d65-b858-765a3ca83376")
+
+    def get_version(self) -> UUID:
+        return version("nbib")
+
+    def get_data_output_category(self) -> DataOutputCategory:
+        return DataOutputCategory.IMMEDIATE
+
+    def get_required_content(self) -> bool:
+        ds_path = Path(self.dataset.path)
+        matching = ds_path.glob("**/*.nbib")
+        cite_files = [Path(res["path"]) for res in self.dataset.get(matching, result_renderer="disabled")]
+        self._cite_files = cite_files
+
+    @staticmethod
+    def _coerce_types(ref) -> dict:
+        cref = ref.copy()
+        # Convert datetime to iso-formatted strings to make them json-serializable
+        for k,v in ref.items():
+            if type(v) is datetime:
+                cref[k] = v.isoformat()
+        return cref
+
+    def _read_files(self) -> list[dict]:
+        refs = []
+        for f in self._cite_files:
+            for ref in nbib.read_file(f):
+                refs.append(self._coerce_types(ref))
         return refs
 
     def extract(self, _=None) -> ExtractorResult:
