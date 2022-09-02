8220e57f39d81b6219221ac5816c63bc2c8b5efa
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2022-09-02 11:01:02 +0200

    Refactor RIS translator
    This makes the translator a class. Still not sure if that's optimal,
    but it gives space for an analogous nbib translator in the same file.

--- datalad_wackyextra/translators/citations.py
+++ datalad_wackyextra/translators/citations.py
@@ -1,67 +1,67 @@
 import json
 
-def translate_ris(metadata_record):
-    refs = metadata_record["extracted_metadata"]["refs"]
-    publications = []
-    for ref in refs:
-        publications.append(
-            {
-                "type": get_type(ref),
-                "title": get_title(ref),
-                "doi": get_doi(ref),
-                "datePublished": get_date_published(ref),
-                "authors": get_authors(ref),
-            }
-        )
-    translated_record = {
-        "type": metadata_record["type"],
-        "dataset_id": metadata_record["dataset_id"],
-        "dataset_version": metadata_record["dataset_version"],
-        "name": "",
-        "publications": publications,
-    }
-    return translated_record
+class RisTranslator:
+    def __init__(self, metadata_record):
+        self.metadata_record = metadata_record
 
+    @staticmethod
+    def _getOneOf(d, *args):
+        for key in args:
+            val = d.get(key)
+            if val is not None:
+                break
+        return val
 
-def _getOneOf(d, *args):
-    for key in args:
-        val = d.get(key)
-        if val is not None:
-            break
-    return val
+    def get_type(self, ref):
+        type_map = {
+            "JOUR": "Journal Article",
+            "CHAP": "Book Section",
+            "THES": "Thesis",
+        }
+        ris_abbrev = ref["type_of_reference"]
+        return type_map.get(ris_abbrev, "Other ({})".format(ris_abbrev))
 
+    def get_title(self, ref):
+        return self._getOneOf(ref, "title", "primary_title")
 
-def get_type(ref):
-    type_map = {
-        "JOUR": "Journal Article",
-        "CHAP": "Book Section",
-        "THES": "Thesis",
-    }
-    ris_abbrev = ref["type_of_reference"]
-    return type_map.get(ris_abbrev, "Other ({})".format(ris_abbrev))
+    def get_doi(self, ref):
+        doi = ref.get("doi")
+        return "https://doi.org/" + doi if doi is not None else ""
 
+    def get_date_published(self, ref):
+        return self._getOneOf(ref, "year", "publication_year")
 
-def get_title(ref):
-    return _getOneOf(ref, "title", "primary_title")
-    
+    def get_authors(self, ref):
+        return [{"name": x} for x in self._getOneOf(ref, "authors", "first_authors")]
 
-def get_doi(ref):
-    doi = ref.get("doi")
-    return "https://doi.org/" + doi if doi is not None else ""
-
-
-def get_date_published(ref):
-    return _getOneOf(ref, "year", "publication_year")
-
-
-def get_authors(ref):
-    return [{"name": x} for x in _getOneOf(ref, "authors", "first_authors")]
+    def translate(self):
+        refs = self.metadata_record["extracted_metadata"]["refs"]
+        publications = []
+        for ref in refs:
+            publications.append(
+                {
+                    "type": self.get_type(ref),
+                    "title": self.get_title(ref),
+                    "doi": self.get_doi(ref),
+                    "datePublished": self.get_date_published(ref),
+                    "authors": self.get_authors(ref),
+                }
+            )
+        translated_record = {
+            "type": self.metadata_record["type"],
+            "dataset_id": self.metadata_record["dataset_id"],
+            "dataset_version": self.metadata_record["dataset_version"],
+            "name": "",
+            "publications": publications,
+        }
+        return translated_record
 
 
 if __name__ == "__main__":
     import sys
     fname = sys.argv[1]
     with open(fname) as jf:
-        j = json.load(jf)
-        t = translate_ris(j)
-        print(json.dumps(t))
+        for line in jf:
+            j = json.loads(line)
+            t = RisTranslator(j).translate()
+            print(json.dumps(t))
